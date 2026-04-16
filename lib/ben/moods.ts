7bd5836f9d6563d85a7e702c009c42f6b2d33594
Kristine Kahler <124746855+KristineMK72@users.noreply.github.com>
2026-04-16 12:40:24 -0500

    Add BenMoods object with mood styles

---
+++ lib/ben/moods.ts
@@ -0,0 +1,7 @@
+export const BenMoods = {
+  encouraging: "border-green-500/30 bg-green-600/20 text-green-100",
+  stern: "border-yellow-600/30 bg-yellow-700/20 text-yellow-100",
+  urgent: "border-orange-600/40 bg-orange-700/30 text-white",
+  witty: "border-blue-500/30 bg-blue-600/20 text-blue-100",
+  celebratory: "border-purple-500/30 bg-purple-600/20 text-purple-100",
+} as const;
