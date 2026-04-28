9d0b30872c11191df6701bcf96ae2422ad7148f7
Kristine Kahler <124746855+KristineMK72@users.noreply.github.com>
2026-04-28 16:10:42 -0500

    Update client.ts

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,10 +1,24 @@
-"use client";
+import { createServerClient } from "@supabase/ssr";
+import { cookies } from "next/headers";
 
-import { createBrowserClient } from "@supabase/ssr";
+export function createSupabaseServerClient() {
+  const cookieStore = cookies();
 
-export function createSupabaseBrowserClient() {
-  return createBrowserClient(
+  return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name) {
+          return cookieStore.get(name)?.value;
+        },
+        set(name, value, options) {
+          cookieStore.set({ name, value, ...options });
+        },
+        remove(name, options) {
+          cookieStore.set({ name, value: "", ...options });
+        },
+      },
+    }
   );
 }
