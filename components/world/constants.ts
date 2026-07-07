4129db0cde798ccbbf146d56538f8f13634d1e76
Kristine Kahler <124746855+KristineMK72@users.noreply.github.com>
2026-07-06 20:25:26 -0500

    Add constants for world configuration

---
+++ components/world/constants.ts
@@ -0,0 +1,12 @@
+export const SPEED = 10;
+export const EYE_HEIGHT = 1.75;
+
+export const MAP_POS: [number, number, number] = [0, 92, 92];
+export const MAP_LOOK_AT: [number, number, number] = [0, 0, -4];
+
+export const FIRST_PERSON_POS: [number, number, number] = [0, EYE_HEIGHT, 34];
+
+export const WORLD_LIMIT_X = 120;
+export const WORLD_LIMIT_Z = 125;
+
+export const CHANNEL = "franklins-landing-v4";
