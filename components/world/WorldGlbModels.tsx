3370cc0709ed9ea315b33d7f0f59f64bb7ae7c6b
Kristine Kahler <124746855+KristineMK72@users.noreply.github.com>
2026-08-28 12:37:19 -0500

    Mount town GLBs and shrink the stone house

---
+++ components/world/WorldGlbModels.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { useGLTF } from "@react-three/drei";
+import { WORLD_ASSETS } from "./worldAssets";
+import type { WorldAsset } from "./types";
+
+const SKIP_ON_MOBILE = new Set([
+  "lightning",
+  "stone-house",
+]);
+
+function GlbAsset({ asset }: { asset: WorldAsset }) {
+  const gltf = useGLTF(asset.url);
+  return (
+    <primitive
+      object={gltf.scene.clone()}
+      position={[asset.x, asset.y ?? 0, asset.z]}
+      rotation={[0, asset.rotY ?? 0, 0]}
+      scale={asset.scale ?? 1}
+    />
+  );
+}
+
+export default function WorldGlbModels({ isMobile }: { isMobile: boolean }) {
+  const assets = WORLD_ASSETS.filter((asset) => {
+    if (isMobile && SKIP_ON_MOBILE.has(asset.id)) return false;
+    return true;
+  });
+
+  return (
+    <group>
+      {assets.map((asset) => (
+        <GlbAsset key={asset.id} asset={asset} />
+      ))}
+    </group>
+  );
+}
+
+for (const asset of WORLD_ASSETS) {
+  if (!SKIP_ON_MOBILE.has(asset.id)) {
+    useGLTF.preload(asset.url);
+  }
+}
